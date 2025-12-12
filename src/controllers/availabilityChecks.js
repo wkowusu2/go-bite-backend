710da6e328120b449eb2fb1e5f0991d5634a5d78
William <tooknown27@gmail.com>
2025-12-12 00:16:27 +0000

    added a route for db pinging

--- src/controllers/availabilityChecks.js
+++ src/controllers/availabilityChecks.js
@@ -1,7 +1,22 @@
+import { sql } from "drizzle-orm"
+import { getDb } from "../config/db.config"
+
 export const home = (req, res) => {
     return res.json("Welcome to the custom backend for goBite")
 }
 
 export const health = (req, res) => {
     return res.json("Server is up and running successfully")
+}
+
+export const pingDb = async (req, res) => {
+   try {
+    const db = getDb(); 
+    await db.execute(sql`SELECT 1`);
+    console.log("Db ping successful"); 
+    return res.status(200).send('DB ping successful')
+   } catch (error) {
+    console.log("Error from db ping: ", error)
+    return res.status(500).send('Error poking DB');
+   }
 }
--- src/routes/health.js
+++ src/routes/health.js
@@ -1,9 +1,11 @@
 import {Router} from 'express';
-import {home, health } from '../controllers/availabilityChecks.js'
+import {home, health, pingDb } from '../controllers/availabilityChecks.js'
 
 const router = Router();
 router.get('/', home )
 
 router.get("/health", health)
+
+router.get('/ping-db', pingDb)
 
 export default router;
